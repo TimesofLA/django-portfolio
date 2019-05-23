832c47f1446fa1b84272a4f92f6fdcd939148380
Keenan Kelsey <keenan.kelsey@gmail.com>
2019-05-23 10:32:11 -0600

    "changed Postgresql name

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
+        'NAME': 'mydb',
         'USER':'postgres',
         'PASSWORD':'django1234',
         'HOST':"localhost",
